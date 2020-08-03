c404ce30e18cd3c69f6e7617f578c95ed14ab992
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-08-03 19:09:17 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,29 @@
+class Artist 
+  attr_accessor :name 
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self 
+  end
+  def self.all 
+    @@all 
+  end
+  def add_song(song)
+    song.artist = self 
+  end
+  def songs 
+    Song.all.select do |element|
+      element.artist == self 
+    end 
+  end
+  def self.find_or_create_by_name(name_given)
+    if self.all.include? {|element| element.name ==   
+  end
+  def print_songs
+    Song.all.each do |element|
+      puts element.name if element.artist == self 
+    end
+  end
+  
+    
+      
---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+class Song 
+  attr_accessor :name, :artist 
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self 
+  end
+  def self.all 
+    @@all 
+  end
+  def self.new_by_file_name(file_name)
+    song_name = file_name.split(" - ")[1]
+    song = Song.new(song_name)
+    song.artist_name = artist  
+    song 
+  end
+  def artist_name=(string)
+     
+    
